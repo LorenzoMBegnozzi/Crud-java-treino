fcc50adf0195e3a33350b9961f0691c03978ae6c
LorenzoBerg <lorenzobegnozzi@hotmail.com>
2025-08-27 21:20:32 -0300

    salvando

--- src/main/java/com/logan/example/service/UserService.java
+++ src/main/java/com/logan/example/service/UserService.java
@@ -1,8 +1,10 @@
 package com.logan.example.service;
 
 import com.logan.example.dto.UserDTO;
+import com.logan.example.entity.User;
 import com.logan.example.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.action.internal.EntityActionVetoException;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,28 @@ public class UserService {
     private final UserRepository repository;
 
     public Page<UserDTO> findAll() {
-         return null;
+        return null;
     }
+
+    public UserDTO findById(final Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new EntityActionVetoException("User not found"));
+    }
+
+    public User create(final UserDTO newUser) {
+        return null;
+    }
+
+    public void put(final Long id, final UserDTO user) {
+        return null;
+    }
+
+    public Page<UserDTO> delete(final Long id) {
+        return null;
+    }
+
+    public find(final Long id) {
+
+    }
+
 }
